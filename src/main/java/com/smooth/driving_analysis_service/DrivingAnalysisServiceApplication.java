7b5be285f73f64cf420854426762f19a5c97179a
Songmi54 <121936797+Songmi54@users.noreply.github.com>
2025-08-05 16:44:30 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/smooth/driving_analysis_service/DrivingAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smooth.driving_analysis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingAnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smooth/driving_analysis_service/DrivingAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smooth.driving_analysis_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
