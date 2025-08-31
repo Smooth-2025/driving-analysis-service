797eae7fc892b94969f880c2e74c5884c35fb5a6
seohee-P <p03061180@gmail.com>
2025-08-31 16:58:30 +0900

    feat: Redis Stream 주행 이벤트 발행 기능 구현
    - 주행 완료 시 이벤트 발행 로직 추가
    - DrivingEventDto 및 Stream 서비스 구현

--- src/main/java/com/smooth/driving_analysis_service/driving/exception/DrivingErrorCode.java
+++ src/main/java/com/smooth/driving_analysis_service/driving/exception/DrivingErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum DrivingErrorCode implements ErrorCode {
 
-    DRIVING_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, 5501, "주행기록을 찾을 수 없습니다.");
+    DRIVING_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, 5501, "주행기록을 찾을 수 없습니다."),
+
+    REDIS_STREAM_PUBLISH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 5701, "이벤트 발행에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer code;
