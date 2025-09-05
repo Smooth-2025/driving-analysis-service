295b2886b921071607c75616861c3616dffa143b
seohee-P <p03061180@gmail.com>
2025-09-06 08:44:35 +0900

    feat : 게이트웨이에서 userId 받아오는 기능 추가

--- src/main/java/com/smooth/driving_analysis_service/timeline/controller/TimeLineController.java
+++ src/main/java/com/smooth/driving_analysis_service/timeline/controller/TimeLineController.java
@@ -1,6 +1,7 @@
 package com.smooth.driving_analysis_service.timeline.controller;
 
 
+import com.smooth.driving_analysis_service.global.auth.AuthenticationUtils;
 import com.smooth.driving_analysis_service.global.common.ApiResponse;
 import com.smooth.driving_analysis_service.timeline.dto.TimeLineResponseDto;
 import com.smooth.driving_analysis_service.timeline.service.TimeLineService;
@@ -23,7 +24,9 @@ public class TimeLineController {
     public ResponseEntity<ApiResponse<TimeLineResponseDto>> getDrivingTimeLine(
             @RequestParam(required = false) String cursor,
             @RequestParam(defaultValue = "10") int limit) {
-        TimeLineResponseDto dto = timeLineService.getDrivingTimeLine(1L, cursor, limit); // TODO: X-User-Id 헤더로 교체
+
+        Long userId = AuthenticationUtils.getCurrentUserIdOrThrow();
+        TimeLineResponseDto dto = timeLineService.getDrivingTimeLine(userId, cursor, limit); // TODO: X-User-Id 헤더로 교체
         return ResponseEntity.ok(ApiResponse.success("주행 타임라인 조회가 완료되었습니다.", dto));
     }
 
@@ -33,7 +36,9 @@ public class TimeLineController {
     public ResponseEntity<ApiResponse<TimeLineResponseDto>> getReportTimeLine(
             @RequestParam(required = false) String cursor,
             @RequestParam(defaultValue = "10") int limit) {
-        TimeLineResponseDto dto = timeLineService.getReportTimeLine(1L, cursor, limit);
+
+        Long userId = AuthenticationUtils.getCurrentUserIdOrThrow();
+        TimeLineResponseDto dto = timeLineService.getReportTimeLine(userId, cursor, limit);
         return ResponseEntity.ok(ApiResponse.success("리포트 타임라인 조회가 완료되었습니다.", dto));
     }
 
@@ -43,7 +48,9 @@ public class TimeLineController {
     public ResponseEntity<ApiResponse<TimeLineResponseDto>> getAllTimeLine(
             @RequestParam(required = false) String cursor,
             @RequestParam(defaultValue = "10") int limit) {
-        TimeLineResponseDto dto = timeLineService.getAllTimeLine(1L, cursor, limit);
+
+        Long userId = AuthenticationUtils.getCurrentUserIdOrThrow();
+        TimeLineResponseDto dto = timeLineService.getAllTimeLine(userId, cursor, limit);
         return ResponseEntity.ok(ApiResponse.success("전체 타임라인 조회가 완료되었습니다.", dto));
     }
 }
