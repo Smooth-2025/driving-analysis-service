74d2c1981f1dfc2247ef70c92bbdc786b3f00424
seohee-P <p03061180@gmail.com>
2025-09-07 15:14:48 +0900

    refactor: 코드 정리 및 애노테이션 추가
    - 불필요한 import 문 제거
    - DrivingCharacterRepository에 @Repository 애노테이션 추가
    - UserDrivingStateRepository에 @Repository 애노테이션 추가

--- src/main/java/com/smooth/driving_analysis_service/driving/controller/CharacterController.java
+++ src/main/java/com/smooth/driving_analysis_service/driving/controller/CharacterController.java
@@ -7,7 +7,6 @@ import com.smooth.driving_analysis_service.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
