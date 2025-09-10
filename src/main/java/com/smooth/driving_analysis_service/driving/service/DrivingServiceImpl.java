5e43eb3c0d8a1926946b7ef7166f18a9fe81aa9d
seohee-P <p03061180@gmail.com>
2025-09-10 20:52:56 +0900

    refactor: 비동기 시 대기 시간 수정

--- src/main/java/com/smooth/driving_analysis_service/driving/service/DrivingServiceImpl.java
+++ src/main/java/com/smooth/driving_analysis_service/driving/service/DrivingServiceImpl.java
@@ -36,7 +36,7 @@ public class DrivingServiceImpl implements DrivingService {
     private final RedisStreamService redisStreamService;
     private final CharacterService characterService;
 
-    private final static int TIME_FOR_WAIT = 150000;
+    private final static int TIME_FOR_WAIT = 70000;
 
     @Async("taskExecutor")
     @Override
